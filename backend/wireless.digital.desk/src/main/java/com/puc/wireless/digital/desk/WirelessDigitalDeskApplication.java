d056e1b22bcc914bbcf0457005b86380f1631c12
Guilherme <gui.pavarina.sa@gmail.com>
2022-05-06 11:05:33 -0300

    creating scripts to start kong + keycloak

---
+++ backend/wireless.digital.desk/src/main/java/com/puc/wireless/digital/desk/WirelessDigitalDeskApplication.java
@@ -0,0 +1,13 @@
+package com.puc.wireless.digital.desk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WirelessDigitalDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WirelessDigitalDeskApplication.class, args);
+	}
+
+}
---
+++ backend/wireless.digital.desk/src/test/java/com/puc/wireless/digital/desk/WirelessDigitalDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puc.wireless.digital.desk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WirelessDigitalDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
